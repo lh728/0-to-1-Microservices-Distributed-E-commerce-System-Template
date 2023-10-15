93c7b58f3b503fc896ed51740ed2726504b02eb4
lh728 <425658167@qq.com>
2023-10-15 14:42:13 +0800

    Initial Project Architecture

---
+++ coupon/src/main/java/com/ecommercesystemtemplate/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercesystemtemplate.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class, args);
+    }
+
+}
