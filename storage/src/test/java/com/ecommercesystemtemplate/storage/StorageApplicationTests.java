c71c5a8614d79241bfd0d54ce6fa2d5332b85fb2
lh728 <425658167@qq.com>
2023-10-15 14:26:55 +0800

    init

---
+++ storage/src/test/java/com/ecommercesystemtemplate/storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercesystemtemplate.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
