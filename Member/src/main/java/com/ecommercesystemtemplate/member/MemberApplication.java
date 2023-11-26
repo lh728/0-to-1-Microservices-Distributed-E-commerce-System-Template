d759152e074c44dab8430b157523dfe64ece1d8e
hongjin <425658167@qq.com>
2023-11-26 22:48:52 +0800

    deploy nacos server - member

--- Member/src/main/java/com/ecommercesystemtemplate/member/MemberApplication.java
+++ Member/src/main/java/com/ecommercesystemtemplate/member/MemberApplication.java
@@ -3,8 +3,11 @@ package com.ecommercesystemtemplate.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @MapperScan("com.ecommercesystemtemplate.member.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MemberApplication {
 
 	public static void main(String[] args) {
