ebef0092588d1bec07d6ddce1cc808b00d0beb92
hongjin <425658167@qq.com>
2023-12-21 19:46:57 +0800

    oss config

--- Product/src/test/java/com/ecommercesystemtemplate/product/ProductApplicationTests.java
+++ Product/src/test/java/com/ecommercesystemtemplate/product/ProductApplicationTests.java
@@ -1,10 +1,22 @@
 package com.ecommercesystemtemplate.product;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import com.ecommercesystemtemplate.product.entity.BrandEntity;
 import com.ecommercesystemtemplate.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class ProductApplicationTests {
@@ -19,6 +31,39 @@ class ProductApplicationTests {
         brandEntity.setDescript("");
         brandService.save(brandEntity);
         System.out.printf("保存成功");
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException, com.aliyuncs.exceptions.ClientException {
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        String bucketName = "0-to-1-microservices-distributed-e-commerce-system-template";
+        String objectName = "test.png";
+        String filePath= "D:\\0-to-1-Microservices-Distributed-E-commerce-System-Template\\Static\\test.png";
+
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, inputStream);
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
     }
 
 }
