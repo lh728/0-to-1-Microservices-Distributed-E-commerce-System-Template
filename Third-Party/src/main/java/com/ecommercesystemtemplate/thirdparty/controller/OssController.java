fb840ee149af294538246a804411537b97ff9322
hongjin <425658167@qq.com>
2023-12-22 19:59:53 +0800

    config OSS service

---
+++ Third-Party/src/main/java/com/ecommercesystemtemplate/thirdparty/controller/OssController.java
@@ -0,0 +1,82 @@
+package com.ecommercesystemtemplate.thirdparty.controller;
+
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+
+    @Resource
+    OSSClient ossClient;
+
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    private String bucket;
+
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessId;
+
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy() {
+
+        // https://0-to-1-microservices-distributed-e-commerce-system-template.oss-cn-beijing.aliyuncs.com/test.png
+        // format https://bucketname.endpoint。
+        String host = "https://" + bucket + "." + endpoint;
+        /*
+        Set up the upload callback URL, which is the callback server address used for communication between the application server and OSS.
+        After the file upload is complete, OSS will send the file upload information to the application server through this callback URL.
+         */
+//        String callbackUrl = "https://192.168.0.0:8888";
+        // Set the prefix for uploading files to OSS; this field can be left empty.
+        // When left empty, files will be uploaded to the root directory of the Bucket.
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format + "/";
+
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+
+        return respMap;
+    }
+}
