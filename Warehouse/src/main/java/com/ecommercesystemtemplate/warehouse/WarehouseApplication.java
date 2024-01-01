368aab4c3b1dd0f38391494fc4acadab6732b25d
hongjin <425658167@qq.com>
2024-01-01 17:50:38 +0800

    warehouse service puzzy search

--- Warehouse/src/main/java/com/ecommercesystemtemplate/warehouse/WarehouseApplication.java
+++ Warehouse/src/main/java/com/ecommercesystemtemplate/warehouse/WarehouseApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.ecommercesystemtemplate.warehouse.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class WarehouseApplication {
 
     public static void main(String[] args) {
--- Warehouse/src/main/java/com/ecommercesystemtemplate/warehouse/service/impl/WareInfoServiceImpl.java
+++ Warehouse/src/main/java/com/ecommercesystemtemplate/warehouse/service/impl/WareInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommercesystemtemplate.warehouse.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,13 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(key)){
+            wrapper.eq("id",key).or().like("name",key).or().like("address",key).or().like("areacode",key);
+        }
         IPage<WareInfoEntity> page = this.page(
-                new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                new Query<WareInfoEntity>().getPage(params),wrapper
         );
 
         return new PageUtils(page);
