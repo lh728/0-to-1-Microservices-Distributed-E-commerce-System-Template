3d0568fd1e49af11cefc5a60ab674175d31ce09e
hongjin <425658167@qq.com>
2024-01-12 20:38:46 +0800

    test elasticsearch

--- ELSearch/src/main/java/com/ecommercesystemtemplate/elsearch/config/ElasticConfig.java
+++ ELSearch/src/main/java/com/ecommercesystemtemplate/elsearch/config/ElasticConfig.java
@@ -1,6 +1,7 @@
 package com.ecommercesystemtemplate.elsearch.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +12,23 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ElasticConfig {
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        COMMON_OPTIONS = builder.build();
+    }
+
     @Bean
     public RestHighLevelClient esRestClient() {
         RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(
                 new HttpHost("192.168.56.10", 9200, "http")));
         return restHighLevelClient;
     }
+
+
+
+
+
+
 }
--- ELSearch/src/test/java/com/ecommercesystemtemplate/elsearch/ElSearchApplicationTests.java
+++ ELSearch/src/test/java/com/ecommercesystemtemplate/elsearch/ElSearchApplicationTests.java
@@ -1,9 +1,17 @@
 package com.ecommercesystemtemplate.elsearch;
 
+import com.alibaba.fastjson.JSON;
+import com.ecommercesystemtemplate.elsearch.config.ElasticConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 public class ElSearchApplicationTests {
@@ -13,6 +21,29 @@ public class ElSearchApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(restHighLevelClient);
+    }
+
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest users = new IndexRequest("users");
+        users.id("1");
+        User user = new User();
+        user.setAge(18);
+        user.setName("the first user");
+        user.setGender("male");
+        String json = JSON.toJSONString(user);
+        users.source(json, XContentType.JSON);
+        // save operation
+        IndexResponse index = restHighLevelClient.index(users, ElasticConfig.COMMON_OPTIONS);
+        // get response
+        System.out.println(index);
+
+    }
+    @Data
+    class User{
+        private String name;
+        private String gender;
+        private Integer age;
     }
 
 
