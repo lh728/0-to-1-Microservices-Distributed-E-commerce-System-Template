64cdb423767c30d2254a6fd04cab3c148b288f08
hongjin <425658167@qq.com>
2024-04-16 19:23:24 +0800

    config threadpool

---
+++ Product/src/main/java/com/ecommercesystemtemplate/product/ThreadPoolConfigProperties.java
@@ -0,0 +1,16 @@
+package com.ecommercesystemtemplate.product;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "thellumall.thread")
+@Component
+@Data
+public class ThreadPoolConfigProperties {
+
+    private Integer coreSize;
+    private Integer maxSize;
+    private Integer keepAliveTime;
+
+}
