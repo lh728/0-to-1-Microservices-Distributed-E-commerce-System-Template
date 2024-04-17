2221901b40307eb237cf42611678f254b9223603
hongjin <425658167@qq.com>
2024-04-17 17:50:15 +0800

    countdown function and viewControll

---
+++ AuthServer/src/main/java/com/ecommercesystemtemplate/authserver/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.ecommercesystemtemplate.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/register.html").setViewName("register");
+    }
+}
---
+++ AuthServer/src/main/java/com/ecommercesystemtemplate/authserver/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.ecommercesystemtemplate.authserver.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+//    @GetMapping("/login.html")
+//    public String loginPage(){
+//        return "login";
+//    }
+//
+//    @GetMapping("/register.html")
+//    public String regPage(){
+//        return "register";
+//    }
+}
