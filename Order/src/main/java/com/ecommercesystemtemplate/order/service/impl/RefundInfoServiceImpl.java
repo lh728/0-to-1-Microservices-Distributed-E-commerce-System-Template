a8aee6ea1a75f6faaa404f4775836f22543db1c4
hongjin <425658167@qq.com>
2024-07-09 22:20:44 +0800

    update namespace

--- Order/src/main/java/com/ecommercesystemtemplate/order/service/impl/RefundInfoServiceImpl.java
+++ Order/src/main/java/com/ecommercesystemtemplate/order/service/impl/RefundInfoServiceImpl.java
@@ -1,16 +1,16 @@
 package com.ecommercesystemtemplate.order.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ecommercesystemtemplate.common.utils.PageUtils;
 import com.ecommercesystemtemplate.common.utils.Query;
-
 import com.ecommercesystemtemplate.order.dao.RefundInfoDao;
 import com.ecommercesystemtemplate.order.entity.RefundInfoEntity;
 import com.ecommercesystemtemplate.order.service.RefundInfoService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("refundInfoService")
