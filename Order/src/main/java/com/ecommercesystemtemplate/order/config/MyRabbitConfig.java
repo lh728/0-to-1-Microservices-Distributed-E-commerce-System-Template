4caf80f8f27b88a153363eb93833ed65cab4e0de
hongjin <425658167@qq.com>
2024-07-10 23:05:04 +0800

    build order service environment

--- Order/src/main/java/com/ecommercesystemtemplate/order/config/MyRabbitConfig.java
+++ Order/src/main/java/com/ecommercesystemtemplate/order/config/MyRabbitConfig.java
@@ -3,27 +3,20 @@ package com.ecommercesystemtemplate.order.config;
 import org.springframework.amqp.core.ReturnedMessage;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 public class MyRabbitConfig {
-    @Autowired
+    final
     RabbitTemplate rabbitTemplate;
 
-    /**
-     * use JSON serialization
-     * @return
-     */
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
-
-        return new Jackson2JsonMessageConverter();
+    public MyRabbitConfig(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
     }
+
+
 
     @PostConstruct
     public void init(){
---
+++ Order/src/main/java/com/ecommercesystemtemplate/order/web/WebController.java
@@ -0,0 +1,14 @@
+package com.ecommercesystemtemplate.order.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/{page}.html")
+    public String listPage(@PathVariable("page") String page) {
+        return page;
+    }
+}
