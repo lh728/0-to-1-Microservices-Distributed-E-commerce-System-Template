58b72afc9a6fa732933804473d08918cdb8ff1ee
hongjin <425658167@qq.com>
2024-07-11 21:30:32 +0800

    integrate spring session

--- Order/src/main/java/com/ecommercesystemtemplate/order/OrderApplication.java
+++ Order/src/main/java/com/ecommercesystemtemplate/order/OrderApplication.java
@@ -5,11 +5,13 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @MapperScan("com.ecommercesystemtemplate.order.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableRabbit
+@EnableRedisHttpSession
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ Order/src/main/java/com/ecommercesystemtemplate/order/config/ThreadPoolConfigProperties.java
@@ -0,0 +1,16 @@
+package com.ecommercesystemtemplate.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "thellumall.thread")
+@Component
+@Data
+public class ThreadPoolConfigProperties {
+
+    private Integer coreSize;
+    private Integer maxSize;
+    private Integer keepAliveTime;
+
+}
