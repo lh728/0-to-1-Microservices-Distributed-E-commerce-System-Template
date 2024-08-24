9ad2dcf8ca52acfa2f78cb77b4cf495463958836
hongjin <425658167@qq.com>
2024-08-24 18:33:50 +0800

    update transactional control

--- Order/src/main/java/com/ecommercesystemtemplate/order/OrderApplication.java
+++ Order/src/main/java/com/ecommercesystemtemplate/order/OrderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @MapperScan("com.ecommercesystemtemplate.order.dao")
@@ -14,6 +15,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRabbit
 @EnableRedisHttpSession
 @EnableFeignClients
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class OrderApplication {
 
     public static void main(String[] args) {
