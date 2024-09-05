7566b5d6d54b9e4c5cf3efe572809c752052e1ca
hongjin <425658167@qq.com>
2024-09-05 18:16:56 +0800

    create delay queue to send message

--- Order/src/main/java/com/ecommercesystemtemplate/order/web/WebController.java
+++ Order/src/main/java/com/ecommercesystemtemplate/order/web/WebController.java
@@ -1,28 +1,46 @@
 package com.ecommercesystemtemplate.order.web;
 
 import com.ecommercesystemtemplate.common.exception.NoStockException;
+import com.ecommercesystemtemplate.order.entity.OrderEntity;
 import com.ecommercesystemtemplate.order.service.OrderService;
 import com.ecommercesystemtemplate.order.vo.OrderConfirmVo;
 import com.ecommercesystemtemplate.order.vo.OrderSubmitVo;
 import com.ecommercesystemtemplate.order.vo.SubmitOrderResponseVo;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Controller
 public class WebController {
     final
     OrderService orderService;
+    final RabbitTemplate rabbitTemplate;
 
-    public WebController(OrderService orderService) {
+    public WebController(OrderService orderService, RabbitTemplate rabbitTemplate) {
         this.orderService = orderService;
+        this.rabbitTemplate = rabbitTemplate;
     }
+
+    @GetMapping("/test/createOrder")
+    @ResponseBody
+    public String createOrderTest(){
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setOrderSn(UUID.randomUUID().toString());
+        orderEntity.setModifyTime(new Date());
+        rabbitTemplate.convertAndSend("order-event-exchange", "order.create.order", orderEntity);
+        return "ok";
+    }
+
 
     @GetMapping("/{page}.html")
     public String listPage(@PathVariable("page") String page) {
