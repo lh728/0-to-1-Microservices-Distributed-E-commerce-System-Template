8f9efcbd0b8dbc23468be1a07019112eae5aad48
hongjin <425658167@qq.com>
2024-12-30 15:01:27 +0000

    logic for adding flash sale products in the backend

--- Coupon/src/main/java/com/ecommercesystemtemplate/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ Coupon/src/main/java/com/ecommercesystemtemplate/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,16 +1,17 @@
 package com.ecommercesystemtemplate.coupon.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ecommercesystemtemplate.common.utils.PageUtils;
 import com.ecommercesystemtemplate.common.utils.Query;
-
 import com.ecommercesystemtemplate.coupon.dao.SeckillSkuRelationDao;
 import com.ecommercesystemtemplate.coupon.entity.SeckillSkuRelationEntity;
 import com.ecommercesystemtemplate.coupon.service.SeckillSkuRelationService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,13 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if (!StringUtils.isEmpty(promotionSessionId)) {
+            queryWrapper.eq("promotion_session_id",promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
-                new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                new Query<SeckillSkuRelationEntity>().getPage(params),queryWrapper
         );
 
         return new PageUtils(page);
