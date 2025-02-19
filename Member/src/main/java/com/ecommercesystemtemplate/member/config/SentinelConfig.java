426a8c1e48658f5ffc4f3be02dd617c80c0f1dd3
hongjin <425658167@qq.com>
2025-02-19 20:13:02 +0000

    Custom flow request control

--- Member/src/main/java/com/ecommercesystemtemplate/member/config/SentinelConfig.java
+++ Member/src/main/java/com/ecommercesystemtemplate/member/config/SentinelConfig.java
@@ -1,4 +1,4 @@
-package com.ecommercesystemtemplate.flashsale.config;
+package com.ecommercesystemtemplate.member.config;
 
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
